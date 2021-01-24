162739baa40e87618700054d6baefc866340cbfd
Andrea <andrecasa91@gmail.com>
2021-01-24 23:01:12 +0100

    latex sources added

---
+++ doc/doc_gen.bat
@@ -0,0 +1,8 @@
+color 0b
+
+del /s /f *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl *.lot *.lof
+
+pdflatex Gaussian_Mixture_Model.tex
+pdflatex Gaussian_Mixture_Model.tex
+
+del /s /f *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl *.lot *.lof
